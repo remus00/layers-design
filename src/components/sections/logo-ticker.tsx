550c6cf6223f88c7195b5b6bdfb825072e3327e8
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-15 18:57:06 +0100

    logo ticker animation

--- src/components/sections/logo-ticker.tsx
+++ src/components/sections/logo-ticker.tsx
@@ -1,5 +1,8 @@
+'use client';
 import { logos } from '@/constants';
+import { motion } from 'motion/react';
 import Image from 'next/image';
+import React from 'react';
 import { SectionWrapper } from '../custom/section-wrapper';
 
 export const LogoTicker = () => {
@@ -9,12 +12,20 @@ export const LogoTicker = () => {
                 <h3 className="text-center text-xl text-white/50">
                     Already chosen by these market leaders
                 </h3>
-                <div className="mt-12 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-                    <div className="flex gap-24 pr-24">
-                        {logos.map((logo) => (
-                            <Image key={logo.name} src={logo.image} alt={logo.name} />
+                <div className="mt-12 flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+                    <motion.div
+                        animate={{ x: '-50%' }}
+                        transition={{ duration: 30, ease: 'linear', repeat: Infinity }}
+                        className="flex gap-24 flex-none pr-24"
+                    >
+                        {Array.from({ length: 2 }).map((_, idx) => (
+                            <React.Fragment key={idx}>
+                                {logos.map((logo) => (
+                                    <Image key={logo.name} src={logo.image} alt={logo.name} />
+                                ))}
+                            </React.Fragment>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </SectionWrapper>
