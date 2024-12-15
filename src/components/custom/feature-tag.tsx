51e0acd9101288052ba50ef39bccbeb5873d692d
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-15 20:04:13 +0100

    features animation

--- src/components/custom/feature-tag.tsx
+++ src/components/custom/feature-tag.tsx
@@ -7,12 +7,12 @@ export const FeatureTag = (props: { label: string } & HTMLAttributes<HTMLDivElem
     return (
         <div
             className={cn(
-                'inline-flex items-center gap-3 rounded-2xl border border-white/10 bg-neutral-900 px-3 py-1.5 md:px-5 md:py-2',
-                className
+                'inline-flex items-center gap-3 rounded-2xl border border-white/10 bg-neutral-900 px-3 group py-1.5 md:px-5 md:py-2 hover:scale-105 transition duration-500',
+                className,
             )}
             {...rest}
         >
-            <span className="inline-flex size-5 items-center justify-center rounded-full bg-lime-400 text-xl text-neutral-950">
+            <span className="inline-flex group-hover:rotate-90 transition duration-500 size-5 items-center justify-center rounded-full bg-lime-400 text-xl text-neutral-950">
                 &#10038;
             </span>
             <span className="font-medium md:text-lg">{label}</span>
--- src/components/sections/features.tsx
+++ src/components/sections/features.tsx
@@ -1,6 +1,7 @@
 import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg';
 import avatar3 from '@/assets/images/avatar-florence-shaw.jpg';
 import avatar2 from '@/assets/images/avatar-lula-meyers.jpg';
+import avatar4 from '@/assets/images/avatar-owen-garcia.jpg';
 import { featuresMap } from '@/constants';
 import Image from 'next/image';
 import { Avatar } from '../custom/avatar';
@@ -23,7 +24,7 @@ export const Features = () => {
                     <FeatureCard
                         title="Real-time Collaboration"
                         description="Work together seamlessly with conflict-free team editing"
-                        className="md:col-span-2 lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 group"
                     >
                         <div className="flex aspect-video items-center justify-center">
                             <Avatar className="z-40">
@@ -35,8 +36,13 @@ export const Features = () => {
                             <Avatar className="z-20 -ml-6 border-amber-500">
                                 <Image src={avatar3} alt="avatar 3" className="rounded-full" />
                             </Avatar>
-                            <Avatar className="z-10 -ml-6 border-transparent">
-                                <div className="flex size-full items-center justify-center gap-1 rounded-full bg-neutral-700">
+                            <Avatar className="z-10 duration-500 -ml-6 border-transparent group-hover:border-green-500 transition">
+                                <div className="flex size-full items-center justify-center gap-1 relative rounded-full bg-neutral-700">
+                                    <Image
+                                        src={avatar4}
+                                        alt="avatar 4"
+                                        className="rounded-full absolute size-full opacity-0 duration-500 group-hover:opacity-100 transition"
+                                    />
                                     {Array.from({ length: 3 }).map((_, idx) => (
                                         <span
                                             key={idx}
@@ -51,13 +57,21 @@ export const Features = () => {
                     <FeatureCard
                         title="Interactive Prototyping"
                         description="Engage your client with prototypes that react to user actions"
-                        className="md:col-span-2 lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 group"
                     >
                         <div className="flex aspect-video items-center justify-center">
-                            <p className="text-center text-4xl font-extrabold text-white/20">
+                            <p className="text-center text-4xl font-extrabold text-white/20 group-hover:text-white/10 duration-500 transition">
                                 We&apos;ve achieved{' '}
-                                <span className="to bg-pink-400 bg-gradient-to-tr from-purple-400 bg-clip-text text-transparent">
-                                    incredible
+                                <span className="to bg-pink-400 relative bg-gradient-to-tr from-purple-400 bg-clip-text text-transparent">
+                                    <span>incredible</span>
+                                    <video
+                                        src="/assets/gif-incredible.mp4"
+                                        autoPlay
+                                        loop
+                                        muted
+                                        playsInline
+                                        className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl opacity-0 pointer-events-none group-hover:opacity-100 transition duration-500"
+                                    />
                                 </span>{' '}
                                 growth this year
                             </p>
@@ -67,12 +81,21 @@ export const Features = () => {
                     <FeatureCard
                         title="Keyboard Quick Actions"
                         description="Powerfull commands to help you create designs quickly"
-                        className="md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto"
+                        className="md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto group"
                     >
                         <div className="flex aspect-video items-center justify-center gap-4">
-                            <Key label="shift" className="w-28" />
-                            <Key label="alt" />
-                            <Key label="C" />
+                            <Key
+                                label="shift"
+                                className="w-28 outline-transparent outline-offset-4 outline group-hover:outline-lime-400 outline-2 transition-all duration-500 group-hover:translate-y-1"
+                            />
+                            <Key
+                                label="alt"
+                                className="outline-transparent outline-offset-4 outline group-hover:outline-lime-400 outline-2 transition-all duration-500 delay-150 group-hover:translate-y-1"
+                            />
+                            <Key
+                                label="C"
+                                className="outline-transparent outline-offset-4 outline group-hover:outline-lime-400 outline-2 transition-all duration-500 delay-300 group-hover:translate-y-1"
+                            />
                         </div>
                     </FeatureCard>
                 </div>
